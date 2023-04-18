68e32c97fb2b7cfd2f31746cf1a090fb3c9c8f98
DJSTRIKANOVA <efx_builder@protonmail.com>
2023-04-17 21:43:01 -0500

    basic search

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import {
   DateRangeFacet,
   MultiMatchQuery,
   RangeFacet,
-  RefinementSelectFacet
+  RefinementSelectFacet,
 } from "@searchkit/sdk";
 import {
   FacetsList,
   SearchBar,
   ResetSearchButton,
   SelectedFilters,
-  Pagination
+  Pagination,
 } from "@searchkit/elastic-ui";
 import { useSearchkitVariables } from "@searchkit/client";
 import { useSearchkitSDK } from "@searchkit/sdk/lib/esm/react-hooks";
@@ -28,145 +28,65 @@ import {
   EuiHorizontalRule,
   EuiText,
   EuiFlexGroup,
-  EuiFlexItem
+  EuiFlexItem,
+  EuiCard,
+  EuiImage,
+  EuiPanel,
+  EuiButton
 } from "@elastic/eui";
 
 import "@elastic/eui/dist/eui_theme_light.css";
+import "./App.css";
 
 const config = {
-  host: "https://commerce-demo.es.us-east4.gcp.elastic-cloud.com:9243",
+  host: "http://localhost:4000",
   connectionOptions: {
-    apiKey: "NWF4c2VYOEJzRDhHMzlEX1JDejU6YnJXaS1XWjlSZ2F5ek1Cc3V4aXV6dw=="
+
   },
-  index: "imdb_movies",
+
+  index: "nfts",
   hits: {
-    fields: ["title"]
+    fields: ["caption_model_vit_l_14_openai_f1","id","ipfs_hash"],
+    size: 25
   },
   query: new MultiMatchQuery({
-    fields: [
-      "title",
-      "genres",
-      "directors",
-      "writers",
-      "actors",
-      "countries",
-      "plot"
-    ]
-  }),
-  facets: [
-    new RefinementSelectFacet({
-      field: "type",
-      identifier: "type",
-      label: "Type",
-      multipleSelect: true
-    }),
-    new RangeFacet({
-      field: "metascore",
-      identifier: "metascore",
-      label: "Metascore",
-      range: {
-        min: 0,
-        max: 100,
-        interval: 5
-      }
-    }),
-    new DateRangeFacet({
-      field: "released",
-      identifier: "released",
-      label: "Released"
-    }),
-
-    new RefinementSelectFacet({
-      field: "genres.keyword",
-      identifier: "genres",
-      label: "Genres",
-      multipleSelect: true
-    }),
-
-    new RefinementSelectFacet({
-      field: "countries.keyword",
-      identifier: "countries",
-      label: "Countries"
-    }),
-    new RefinementSelectFacet({
-      field: "rated",
-      identifier: "rated",
-      label: "Rated",
-      multipleSelect: true
-    }),
-    new RefinementSelectFacet({
-      field: "directors.keyword",
-      identifier: "directors",
-      label: "Directors"
-    }),
-
-    new RefinementSelectFacet({
-      field: "writers.keyword",
-      identifier: "writers",
-      label: "Writers"
-    }),
-
-    new RefinementSelectFacet({
-      field: "actors.keyword",
-      identifier: "actors",
-      label: "Actors",
-      multipleSelect: true
-    }),
-
-    new RangeFacet({
-      field: "imdbrating",
-      identifier: "imdbrating",
-      label: "IMDB Rating",
-      range: {
-        interval: 1,
-        max: 10,
-        min: 1
-      }
-    })
-  ]
+    fields: ["caption_model_vit_l_14_openai_f1"],
+  })
 };
 
 const HitsList = ({ data }) => (
-  <EuiFlexGrid>
+  <EuiFlexGrid columns={3} gutterSize="xl">
     {data?.hits.items.map((hit) => (
+
       <EuiFlexItem key={hit.id}>
         <EuiFlexGroup gutterSize="xl">
-          <EuiFlexItem>
-            <EuiFlexGroup>
-              <EuiFlexItem grow={false}>
-                <img
-                  src={hit.fields.poster}
-                  alt="Nature"
-                  style={{ height: "150px" }}
+          <EuiFlexItem grow={true} wrapText
+                       style={{ maxWidth: "300px" }}>
+                <EuiImage
+                    caption={
+                      hit.highlight && hit.highlight.caption_model_vit_l_14_openai_f1
+                          ? <span dangerouslySetInnerHTML={{ __html: hit.highlight.caption_model_vit_l_14_openai_f1 }} />
+                          : hit.fields.caption_model_vit_l_14_openai_f1 || "No field found"
+                    }
+                    src={"https://atomichub-ipfs.com/ipfs/" + hit.fields.ipfs_hash}
+                    style={{ maxWidth: "300px" }}
+                    size="l"
+                    allowFullScreen
+                    fullScreenIconColor="dark"
                 />
               </EuiFlexItem>
-              <EuiFlexItem grow={4}>
-                <EuiTitle size="xs">
-                  <h6>{hit.fields.title}</h6>
-                </EuiTitle>
-                <EuiText grow={false}>
-                  <p>{hit.fields.plot}</p>
-                </EuiText>
-              </EuiFlexItem>
-              <EuiFlexItem grow={2}>
-                <EuiText grow={false}>
-                  <ul>
-                    <li>
-                      <b>ACTORS: </b>
-                      {hit.fields.actors.join(", ")}
-                    </li>
+          {/*<EuiFlexItem>*/}
+          {/*  <a href={"https://atomichub-ipfs.com/ipfs/"+hit.id} target="_blank" rel="noreferrer noopener">*/}
+          {/*    View Image*/}
+          {/*  </a>*/}
+          {/*  <p>{hit.id}</p>*/}
+          {/*</EuiFlexItem>*/}
+          <EuiFlexItem>
 
-                    <li>
-                      <b>WRITERS: </b>
-                      {hit.fields.writers.join(", ")}
-                    </li>
-                  </ul>
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiFlexItem>
+
     ))}
   </EuiFlexGrid>
 );
